2b9859ace9eb22feae9eddf5abbf55fad744d8df
ahmedsalemgithub <ahmedsalem.developer@gmail.com>
2020-02-28 15:21:38 +0200

    Initial commit

---
+++ android/app/src/main/java/online/thecoder/park_bark/MainActivity.java
@@ -0,0 +1,13 @@
+package online.thecoder.park_bark;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
